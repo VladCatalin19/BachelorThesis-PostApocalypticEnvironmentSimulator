6181757c45f52cc779ffd66b9303c67d31daabed
Vlad Marius <23134616+VladCatalin19@users.noreply.github.com>
2026-09-23 13:31:05 +0300

    Fixed a bug that made the project not compile.

--- Assets/Scripts/Ivy/IvyBehavior.cs
+++ Assets/Scripts/Ivy/IvyBehavior.cs
@@ -6,54 +6,55 @@ using UnityEngine;
 namespace Ivy
 {
     [HelpURL("https://github.com/radiatoryang/hedera/wiki")]
-    public class IvyBehavior : MonoBehaviour {
-
-    public List<IvyGraph> ivyGraphs = new List<IvyGraph>();
-    public bool generateMeshDuringGrowth = true, enableGrowthSim = true;
-    public bool showProfileFoldout;
-    public IvyProfileAsset profileAsset;
-    public Color debugColor = Color.yellow;
-
-    private float lastRefreshTime = 0.0f;
-    public float refreshInterval;
-    public float probability;
-
-    private Vector3 minCoords = new Vector3(-3.4f, 0.0076f, -2.5f);
-    private Vector3 maxCoords = new Vector3(+3.4f, 0.0076f, +2.5f);
-
-    private void Start()
+    public class IvyBehavior : MonoBehaviour
     {
-        Time.timeScale = 1;
-        IvyCore _ = new IvyCore();
+        public List<IvyGraph> ivyGraphs = new List<IvyGraph>();
+        public bool generateMeshDuringGrowth = true, enableGrowthSim = true;
+        public bool showProfileFoldout;
+        public IvyProfileAsset profileAsset;
+        public Color debugColor = Color.yellow;
 
-        foreach ((Vector3 position, Vector3 normal) in GetRoots())
+        private float lastRefreshTime = 0.0f;
+        public float refreshInterval;
+        public float probability;
+
+        private Vector3 minCoords = new Vector3(-3.4f, 0.0076f, -2.5f);
+        private Vector3 maxCoords = new Vector3(+3.4f, 0.0076f, +2.5f);
+
+        private void Start()
         {
-            //Debug.Log($"This position: {transform.position}, Root position: {root.transform.position}");
-            IvyGraph ivyGraph = IvyCore.SeedNewIvyGraph(profileAsset.ivyProfile, position, Vector3.up, normal, transform, true);
-            ivyGraph.isGrowing = true;
-            ivyGraphs.Add(ivyGraph);
+            Time.timeScale = 1;
+            IvyCore _ = new IvyCore();
 
-            float branchPercentage = Mathf.Clamp(ivyGraph.roots[0].nodes.Last().cS / profileAsset.ivyProfile.maxLength, 0f, 0.38f);
-            int branchCount = Mathf.FloorToInt(profileAsset.ivyProfile.maxBranchesTotal * branchPercentage * profileAsset.ivyProfile.branchingProbability);
-            for (int b = 0; b < branchCount; b++)
+            foreach ((Vector3 position, Vector3 normal) in GetRoots())
             {
-                IvyCore.ForceRandomIvyBranch(ivyGraph, profileAsset.ivyProfile);
+                //Debug.Log($"This position: {transform.position}, Root position: {root.transform.position}");
+                IvyGraph ivyGraph = IvyCore.SeedNewIvyGraph(profileAsset.ivyProfile, position, Vector3.up, normal, transform, true);
+                ivyGraph.isGrowing = true;
+                ivyGraphs.Add(ivyGraph);
+
+                float branchPercentage = Mathf.Clamp(ivyGraph.roots[0].nodes.Last().cS / profileAsset.ivyProfile.maxLength, 0f, 0.38f);
+                int branchCount = Mathf.FloorToInt(profileAsset.ivyProfile.maxBranchesTotal * branchPercentage * profileAsset.ivyProfile.branchingProbability);
+                for (int b = 0; b < branchCount; b++)
+                {
+                    IvyCore.ForceRandomIvyBranch(ivyGraph, profileAsset.ivyProfile);
+                }
             }
-        }
 
             //Debug.Log($"Mask: {LayerMask.GetMask("Default", "UserLayerB")}");
-    }
+        }
 
 
-    private void Update()
-    {
-        if (Time.timeSinceLevelLoad > lastRefreshTime + refreshInterval)
+        private void Update()
         {
-            //Debug.Log($"Time: {Time.time} Last Refresh Time: {lastRefreshTime} Refresh Interval: {refreshInterval}");
-            lastRefreshTime = Time.timeSinceLevelLoad;
-            IvyCore.Update(this, probability);
-            refreshInterval += Time.deltaTime * 0.002f;
-            //Debug.Log($"Refresh Interval: {refreshInterval}");
+            if (Time.timeSinceLevelLoad > lastRefreshTime + refreshInterval)
+            {
+                //Debug.Log($"Time: {Time.time} Last Refresh Time: {lastRefreshTime} Refresh Interval: {refreshInterval}");
+                lastRefreshTime = Time.timeSinceLevelLoad;
+                IvyCore.Update(this, probability);
+                refreshInterval += Time.deltaTime * 0.002f;
+                //Debug.Log($"Refresh Interval: {refreshInterval}");
+            }
         }
 
         private List<(Vector3, Vector3)> GetRoots()
